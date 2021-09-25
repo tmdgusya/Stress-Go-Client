3cdf6fee13a085c36ad3b5502674a7d8b35e2782
roach <tmdgusya@naver.com>
2021-09-25 18:08:44 +0900

    connection 떨어지면 error 리턴하게

--- src/connection/con.go
+++ src/connection/con.go
@@ -5,12 +5,13 @@ import (
 	"net"
 )
 
+const SERVER_DOMAIN = "http://localhost"
 const SOCKET_SERVER_PORT = ":8080"
 
-func ConnectFactory() net.Conn {
-	conn, err := net.Dial("tcp", SOCKET_SERVER_PORT)
+func ConnectFactory() (net.Conn, error) {
+	conn, err := net.Dial("tcp", SERVER_DOMAIN+SOCKET_SERVER_PORT)
 	if err != nil {
 		log.Println(err)
 	}
-	return conn
+	return conn, err
 }
