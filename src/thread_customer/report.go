bb0dcfd2b0679cdf087b05b9c896ea12945b90a5
roach <tmdgusya@naver.com>
2021-09-25 18:28:23 +0900

    test report struct 생성

--- src/thread_customer/report.go
+++ src/thread_customer/report.go
@@ -4,3 +4,23 @@ type Report struct {
 	fail_count int
 	success_count int
 }
+
+func (r *Report) SuccessTestResult() {
+	r.fail_count += 1
+}
+
+func (r *Report) FailTestResult() {
+	r.success_count += 1
+}
+
+func (r *Report) GetSuccessCount() int {
+	return r.success_count
+}
+
+func (r *Report) GetFailCount() int {
+	return r.fail_count
+}
+
+func ReportFactory() *Report {
+	return &Report {fail_count: 0, success_count: 0};
+}
