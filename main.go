3b67a0c1c36b7b197362686fa4d21e24614cd647
roach <tmdgusya@naver.com>
2021-09-26 19:50:53 +0900

    host, token 이동

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"net/url"
 
 	stomp "github.com/drawdy/stomp-ws-go"
@@ -45,18 +44,13 @@ func main() {
 
 	u := url.URL{
 		Scheme: "ws",
-		Host:   "localhost:8761",
+		Host:   "127.0.0.1:8761",
 		Path:   "/stomp",
 	}
 
-	hh := make(http.Header);
-
-	hh["token"] = []string{"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJjbGVhbmluZ19sYWIiLCJleHAiOjE2MzI3MzI3MTYsInRva2VuX2RhdGEiOnsiYXBwSWQiOjAsImFsbG93T3JpZ2lucyI6bnVsbH19.w1ct3ZC7impouFSOAeLR2mJBMF67zmpb1VKys2ce730CFA7t1uO-nmPThQ1XiEFvQ1GpX6FDqloFMDi96JDJM7WinyDYPxvOVjXkySRu1S3_d4qVe_iyNI6G4w31mJ4RJFxSRi8sj3PugeSGadC0I75L_t7o2vQOnPXDOp1KrymYHWaratdCpNYxKL5xC12jqW6gUGlXc8FvyrflDriClfawjzD6ni38-SLixaDLrtWML5claI28BKJQqu3Gvb_duOWhBnLgwXQQfveMxTbwllqBmpc10SdBOS6Tp-bUjRE0usRpnhnRG_Nhg8KptKQUPg4S6ggNg4jVBp7YZ3xtjg"}
-	hh["host"] = []string{"development-app-0"}
-
 	log.Println(u.String())
 
-	conn, resp, err := websocket.DefaultDialer.Dial(u.String(), hh)
+	conn, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatalf("couldn't connect to %v: %v", u.String(), err)
 	}
@@ -67,14 +61,17 @@ func main() {
 		stomp.HK_ACCEPT_VERSION, "1.2,1.1,1.0",
 		stomp.HK_HEART_BEAT, "3000,3000",
 		stomp.HK_HOST, u.Host,
-	}
+		"token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJjbGVhbmluZ19sYWIiLCJleHAiOjE2MzI3MzI3MTYsInRva2VuX2RhdGEiOnsiYXBwSWQiOjAsImFsbG93T3JpZ2lucyI6bnVsbH19.w1ct3ZC7impouFSOAeLR2mJBMF67zmpb1VKys2ce730CFA7t1uO-nmPThQ1XiEFvQ1GpX6FDqloFMDi96JDJM7WinyDYPxvOVjXkySRu1S3_d4qVe_iyNI6G4w31mJ4RJFxSRi8sj3PugeSGadC0I75L_t7o2vQOnPXDOp1KrymYHWaratdCpNYxKL5xC12jqW6gUGlXc8FvyrflDriClfawjzD6ni38-SLixaDLrtWML5claI28BKJQqu3Gvb_duOWhBnLgwXQQfveMxTbwllqBmpc10SdBOS6Tp-bUjRE0usRpnhnRG_Nhg8KptKQUPg4S6ggNg4jVBp7YZ3xtjg",
+		"host",  "development-app-0"}
 	sc, err := stomp.ConnectOverWS(conn, h)
 	if err != nil {
 		log.Fatalf("couldn't create stomp connection: %v", err)
 	}
 
-	mdCh, err := sc.Subscribe(stomp.Headers{stomp.HK_DESTINATION, "/topic/chat"})
-	
+	mdCh, err := sc.Subscribe(stomp.Headers{
+		stomp.HK_DESTINATION, "/topic/chat",
+		stomp.HK_ID, stomp.Uuid(),
+	})
 	if err != nil {
 		log.Fatalf("failed to suscribe greeting message: %v", err)
 	}
